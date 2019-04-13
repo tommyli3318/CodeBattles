01dbac3a87cc44249cd30b2bb24dfa735b2e428f
Ryan M <ryanm@Ryans-MacBook-Pro-2.local>
2019-04-12 23:06:31 -0700

    Added Solution Button

--- frontend/src/components/CodingPage.js
+++ frontend/src/components/CodingPage.js
@@ -1,24 +1,48 @@
 import React, { Component } from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import AppBar from 'material-ui/AppBar'
+import RaisedButton from 'material-ui/RaisedButton'
+
+//Text Editor
 import {UnControlled as CodeMirror} from 'react-codemirror2'
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/material.css';
+
+import axios from 'axios';
 require('codemirror/mode/python/python');
 require('codemirror/mode/javascript/javascript');
 
 export class CodingPage extends Component {
+
+    state = {
+        startValue: 'Enter solution here.',
+        mode: 'python',
+    }
+
+    continue = (e) => {
+        e.preventDefault();
+        console.log('hi')
+        axios.get(`ENTER URL HERE`)
+            .then(res => {
+            const persons = res.data;
+            this.setState({ persons });
+            })
+        //this.props.nextStep();
+    }
    
   render() {
+    const {startValue} = this.state;
+    const {mode} = this.state;
+
     return (
         <MuiThemeProvider>
             <React.Fragment>
                 <AppBar title = 'Code Mirror'></AppBar>
                 <CodeMirror
                   style = {styles.editor}
-                  value='Enter solution here!'
+                  value= {startValue}
                   options={{
-                    mode: 'python',
+                    mode: {mode},
                     theme: 'material',
                     lineNumbers: true,
                     smartIndent: true,
@@ -27,6 +51,12 @@ export class CodingPage extends Component {
                   onChange={(editor, data, value) => {
                   }}
                 />
+                <RaisedButton
+                    label = 'Submit Solution'
+                    primary = {true}
+                    style = {styles.button}
+                    onClick = {this.continue}
+                ></RaisedButton>
             </React.Fragment>
         </MuiThemeProvider>
     )
@@ -34,10 +64,13 @@ export class CodingPage extends Component {
 }
 
 const styles = {
-  editor: {
-      margin: 100,
-      padding: 20
-  }
+    editor: {
+        border: 1,
+        height: 'auto'
+    },
+    button: {
+        margin: 15
+    }
 }
 
 export default CodingPage
